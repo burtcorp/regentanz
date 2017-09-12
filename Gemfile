db3d379eaa99ff41b1577ff19423cee5bb80c621
David Dahl <david@burtcorp.com>
2017-09-12 11:02:33 +0200

    Update to use aws-sdk v3

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
-gem 'aws-sdk-core'
+gem 'aws-sdk-s3', '~> 1'
+gem 'aws-sdk-cloudformation', '~> 1'
 
 group 'test' do
   gem 'rspec'
--- lib/regentanz.rb
+++ lib/regentanz.rb
@@ -1,6 +1,7 @@
 require 'json'
 require 'yaml'
-require 'aws-sdk-core'
+require 'aws-sdk-s3'
+require 'aws-sdk-cloudformation'
 
 module Regentanz
   Error = Class.new(StandardError)
