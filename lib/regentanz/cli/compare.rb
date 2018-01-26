a9be1f097454b5766e8127e6ffbbee5f250ccd82
Theo <theo@iconara.net>
2018-01-26 20:48:02 +0100

    Move the cli classes into the Regentanz::Cli module
    And rename them from Main to Compare and Compile

--- lib/regentanz/cli/compare.rb
+++ lib/regentanz/cli/compare.rb
@@ -1,86 +1,90 @@
 require 'regentanz'
 require 'regentanz/cli/common'
 
-class Diff
-  def initialize(new_object, old_object)
-    @new_object = new_object
-    @old_object = old_object
-  end
+module Regentanz
+  module Cli
+    class Diff
+      def initialize(new_object, old_object)
+        @new_object = new_object
+        @old_object = old_object
+      end
 
-  def to_json(*args)
-    %(\e[31m#{@old_object.to_json(*args)}\e[m\e[32m#{@new_object.to_json(*args)}\e[m)
-  end
-end
-
-class Main
-  include Regentanz::Cli::Common
-
-  def initialize
-    @cf_client = Aws::CloudFormation::Client.new(region: region)
-    @compiler = Regentanz::TemplateCompiler.new(cloud_formation_client: @cf_client)
-  end
-
-  def run(args)
-    load_config
-    stack_name, stack_path, _ = *args
-    new_template = @compiler.compile_from_path(stack_path)
-    @compiler.validate_template(stack_path, new_template.to_json)
-    old_template = get_template(stack_name)
-    diff = compare(new_template, old_template)
-    if diff.to_json != new_template.to_json
-      output = JSON.pretty_generate(diff)
-      puts(output)
-      1
-    else
-      0
+      def to_json(*args)
+        %(\e[31m#{@old_object.to_json(*args)}\e[m\e[32m#{@new_object.to_json(*args)}\e[m)
+      end
     end
-  rescue Regentanz::Error => e
-    $stderr.puts(e.message)
-    2
-  end
 
-  private
+    class Compare
+      include Common
 
-  def region
-    ENV['AWS_REGION'] || ENV['AWS_DEFAULT_REGION'] || 'eu-west-1'
-  end
+      def initialize
+        @cf_client = Aws::CloudFormation::Client.new(region: region)
+        @compiler = Regentanz::TemplateCompiler.new(cloud_formation_client: @cf_client)
+      end
 
-  def get_template(stack_name)
-    YAML.load(@cf_client.get_template(stack_name: stack_name)[:template_body])
-  rescue Aws::Errors::MissingCredentialsError => e
-    raise Regentanz::Error, 'Retrieving template requires AWS credentials', e.backtrace
-  end
-
-  def compare(new_template, old_template)
-    if new_template.is_a?(Date) && old_template.is_a?(String)
-      new_template = new_template.to_s
-    end
-    if new_template.class != old_template.class
-      Diff.new(new_template, old_template)
-    else
-      case new_template
-      when Hash
-        result = {}
-        new_template.each do |(key, value)|
-          result[key] = compare(value, old_template[key])
+      def run(args)
+        load_config
+        stack_name, stack_path, _ = *args
+        new_template = @compiler.compile_from_path(stack_path)
+        @compiler.validate_template(stack_path, new_template.to_json)
+        old_template = get_template(stack_name)
+        diff = compare(new_template, old_template)
+        if diff.to_json != new_template.to_json
+          output = JSON.pretty_generate(diff)
+          puts(output)
+          1
+        else
+          0
         end
-        old_template.each do |(key, value)|
-          result[key] = compare(nil, value) unless new_template.key?(key)
+      rescue Regentanz::Error => e
+        $stderr.puts(e.message)
+        2
+      end
+
+      private
+
+      def region
+        ENV['AWS_REGION'] || ENV['AWS_DEFAULT_REGION'] || 'eu-west-1'
+      end
+
+      def get_template(stack_name)
+        YAML.load(@cf_client.get_template(stack_name: stack_name)[:template_body])
+      rescue Aws::Errors::MissingCredentialsError => e
+        raise Regentanz::Error, 'Retrieving template requires AWS credentials', e.backtrace
+      end
+
+      def compare(new_template, old_template)
+        if new_template.is_a?(Date) && old_template.is_a?(String)
+          new_template = new_template.to_s
         end
-        result
-      when Array
-        if new_template.size != old_template.size
+        if new_template.class != old_template.class
           Diff.new(new_template, old_template)
         else
-          new_template.zip(old_template).map do |(left, right)|
-            compare(left, right)
+          case new_template
+          when Hash
+            result = {}
+            new_template.each do |(key, value)|
+              result[key] = compare(value, old_template[key])
+            end
+            old_template.each do |(key, value)|
+              result[key] = compare(nil, value) unless new_template.key?(key)
+            end
+            result
+          when Array
+            if new_template.size != old_template.size
+              Diff.new(new_template, old_template)
+            else
+              new_template.zip(old_template).map do |(left, right)|
+                compare(left, right)
+              end
+            end
+          else
+            if new_template == old_template
+              new_template
+            else
+              Diff.new(new_template, old_template)
+            end
           end
-        end
-      else
-        if new_template == old_template
-          new_template
-        else
-          Diff.new(new_template, old_template)
         end
       end
     end
--- lib/regentanz/cli/compile.rb
+++ lib/regentanz/cli/compile.rb
@@ -1,26 +1,30 @@
 require 'regentanz'
 require 'regentanz/cli/common'
 
-class Main
-  include Regentanz::Cli::Common
+module Regentanz
+  module Cli
+    class Compile
+      include Common
 
-  def initialize
-    @compiler = Regentanz::TemplateCompiler.new
-  end
+      def initialize
+        @compiler = Regentanz::TemplateCompiler.new
+      end
 
-  def run(args)
-    load_config
-    stack_path = args.first
-    template = @compiler.compile_from_path(stack_path)
-    template_json = JSON.pretty_generate(template)
-    if template_json.bytesize >= 51200
-      template_json = JSON.generate(template)
+      def run(args)
+        load_config
+        stack_path = args.first
+        template = @compiler.compile_from_path(stack_path)
+        template_json = JSON.pretty_generate(template)
+        if template_json.bytesize >= 51200
+          template_json = JSON.generate(template)
+        end
+        @compiler.validate_template(stack_path, template_json)
+        puts(template_json)
+        0
+      rescue Regentanz::Error => e
+        $stderr.puts(e.message)
+        1
+      end
     end
-    @compiler.validate_template(stack_path, template_json)
-    puts(template_json)
-    0
-  rescue Regentanz::Error => e
-    $stderr.puts(e.message)
-    1
   end
 end
