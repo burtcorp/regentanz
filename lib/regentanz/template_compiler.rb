a3db1a683b31bc4ce6980ec5b8f1163ed571ea80
Gustav Munkby <grddev@gmail.com>
2018-03-01 16:18:45 +0100

    Move YAML extensions to separate file

--- lib/regentanz/template_compiler.rb
+++ lib/regentanz/template_compiler.rb
@@ -119,20 +119,6 @@ module Regentanz
       end
     end
 
-    YAML.add_domain_type('regentanz', 'GetAtt') do |_, value|
-      {'Fn::GetAtt' => value.to_s.split('.')}
-    end
-    %w[Ref ResolveRef].each do |name|
-      YAML.add_domain_type('regentanz', name) do |tag, value|
-        {name => value}
-      end
-    end
-    %w[FindInMap GetAZs ImportValue Join Select Split Sub].each do |name|
-      YAML.add_domain_type('regentanz', name) do |tag, value|
-        {['Fn', name].join('::') => value}
-      end
-    end
-
     def load(path)
       YAML.load_file(path)
     rescue Psych::SyntaxError => e
